23b0d9742fdf0082f0269dea339865025a6e6602
aqu1les <felipebarros.dev@gmail.com>
2019-11-13 00:26:34 -0300

    changed Discipline seeder & feedback migration

--- database/seeds/DisciplinesTableSeeder.php
+++ database/seeds/DisciplinesTableSeeder.php
@@ -14,235 +14,235 @@ class DisciplinesTableSeeder extends Seeder
     {
         DB::table("disciplines")->insert([
             "name" => "Desenvolvimento Pessoal e Empregabilidade",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 1
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Informática Básica",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 2
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Lógica Matemática",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 3
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Matemática Aplicada",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 4
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Sistemas Computacionais",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 5
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Banco de Dados",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 6
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Estatística Básica",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 7
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Introdução à administração",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 8
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Lógica de programação",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 9
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Organização e Arquitetura de computadores",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 10
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Programação",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 11
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Banco de Dados Avançado",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 12
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Estrutura de Dados",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 13
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Programação Avançada",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 14
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Sistemas Operacionais",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 15
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Metodologia de Pesquisa",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 16
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Engenharia de Software",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 17
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Fundamentos de Linguagem Formais e Autômatos",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 18
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Redes de Computadores",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 19
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Tópicos Integradores I",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 20
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Empreendedorismo",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 21
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Engenharia de Requisitos",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 22
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Inteligência Artificial",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 23
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Metodologia de Desenvolvimento de Sistemas",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 24
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Redes de Computadores Avançado",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 25
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Usabilidade e Interface",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 26
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Análise e Modelagem de Sistemas",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 27
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Desenvolvimento de Aplicações para Internet",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 28
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Responsabilidade Socioambiental",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 29
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Sistemas Multimídia",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 30
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Tópicos Integradores II",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 31
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Segurança da Informação",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 32
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Desenvolvimento de Aplicações para Dispositivos Móveis",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 33
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Arquitetura de Software",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 34
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Gestão de Projetos",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 35
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Qualidade de Software",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 36
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Teste de Software",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 37
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Sistemas de Telecomunicações",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 38
         ]);
 
         DB::table("disciplines")->insert([
             "name" => "Sistemas Distribuídos",
-            "difficulty" => "Medium",
+            "difficulty" => "Médio",
 			"teacher_id" => 39
         ]);
 
